9a9873393809d1ab8393d23c874da376fc967791
Richard Wotzlaw <r.wotzlaw@gmail.com>
2026-06-14 22:33:53 +0200

    gradient image

---
+++ image-processing/gradient.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+image_width = 256 * 3
+destination_image = Image.new("RGB", (image_width, image_width))
+
+for y in range(image_width):
+    for x in range(image_width):
+        color = (x % 256, y % 256, 128)
+
+        destination_image.putpixel((x, y), color)
+        
+destination_image.show()
